11e30c9007a761056af1fbf61e9aa46cd3e78231
jay-lab-tech <azharnr49@gmail.com>
2026-05-01 23:17:09 +0700

    a

--- database/generator/generator.php
+++ database/generator/generator.php
@@ -13,17 +13,22 @@ return new class implements Generator
      */
     public function run()
     {
-        $user = User::find('user@user.com', 'email');
+        $email = env('ADMIN_EMAIL', 'admin@studio.com');
+        $name = env('ADMIN_NAME', 'Studio Undangan');
+        $password = env('ADMIN_PASSWORD', 'ChangeMe123!');
+
+        $user = User::find($email, 'email');
 
         if (!$user->exist()) {
             $user = User::create([
-                'name' => 'User',
-                'email' => 'user@user.com',
-                'password' => Hash::make('12345678')
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password)
             ]);
         }
 
         $user->fill([
+            'name' => $name,
             'is_filter' => true,
             'is_active' => true,
             'is_confetti_animation' => true,
